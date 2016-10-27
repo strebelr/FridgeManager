034645b4ead7a5f258588f924cd1cef657ec756f
Kazuki Fukushima <kazukifukushima23@gmail.com>
2016-10-27 14:34:01 -0700

    Modified TextRecognitionInteraction

--- app/src/main/java/com/cpen321/fridgemanager/TextRecognitionInteraction.java
+++ app/src/main/java/com/cpen321/fridgemanager/TextRecognitionInteraction.java
@@ -27,13 +27,38 @@ public class TextRecognitionInteraction {
     }
 
     /*
-      TODO: NEEDS TO HANDLE CASE WHEN TEXTBLOCK CONTAINS MORE THAN ONE FOOD.
-      Adds the food to storage if the name is a valid food.
+      Add foods to storage. The method will split the string if it is a sentence containing multiple food items.
+      @param string to try to add
      */
     public void addFoodToStorage(String name) {
+        String[] words = name.replaceAll("[^a-zA-Z ]",
+                "").split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            // You may want to check for a non-word character before blindly
+            // performing a replacement
+            // It may also be necessary to adjust the character class
+            words[i] = words[i].replaceAll("[^\\w]", "");
+            addIndividualFoods(words[i]);
+        }
+    }
+
+    /*
+      Add individual food to storage if it is a valid
+food.
+      @param string must not be a sentence containing
+multiple food items
+     */
+    public void addIndividualFoods(String name) {
+        String real_name;
         if (isFood(name)) {
             di.writeToStorage(name);
         }
+        //else {
+        //    real_name = isAbb(name);
+        //    if (real_name != null) {
+        //        di.writeToStorage(real_name);
+        //    }
+        //}
     }
 
     /*
@@ -54,6 +79,28 @@ public class TextRecognitionInteraction {
             }
         } catch (JSONException e) {}
         return false;
+    }
+
+    /*
+      Checks if Food with given name is valid.
+      @param name of the food
+      @returns true if valid food
+               false otherwise
+     */
+    public String isAbb(String name) {
+        // Iterate the jsonArray and print the info of JSONObjects
+        if(library == null) return null;
+        try {
+            for (int i = 0; i < library.length(); i++) {
+                JSONObject jsonObject = library.getJSONObject(i);
+                String library_abb = jsonObject.optString("abb").toString();
+                if (library_abb != null) {
+                    if (library_abb.toLowerCase().equals(name.toLowerCase()))
+                        return jsonObject.optString("name").toString();;
+                }
+            }
+        } catch (JSONException e) {}
+        return null;
     }
 
     /*
