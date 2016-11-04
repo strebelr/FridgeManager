df3a2794b93e5dd9cc14f43c35ee0614198d6763
Kazuki Fukushima <kazukifukushima23@gmail.com>
2016-11-04 09:34:10 -0700

    Fixed Bug in Main UI

--- app/src/main/java/com/cpen321/fridgemanager/MainMenu.java
+++ app/src/main/java/com/cpen321/fridgemanager/MainMenu.java
@@ -53,6 +53,7 @@ public class MainMenu extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
         setupTabIcons();
 
+        new DatabaseInteraction(getApplicationContext()).setUp();
     }
 
     @Override
