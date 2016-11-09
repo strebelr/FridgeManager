32a750728aaecbec6f3b70bb60133b678d2130e0
Kazuki Fukushima <kazukifukushima23@gmail.com>
2016-11-08 17:35:59 -0800

    minor fix

--- app/src/main/java/com/cpen321/fridgemanager/Notification/AlertReceiver.java
+++ app/src/main/java/com/cpen321/fridgemanager/Notification/AlertReceiver.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
-import com.cpen321.fridgemanager.MainMenu;
+import com.cpen321.fridgemanager.Activity.MainMenu;
 import com.cpen321.fridgemanager.R;
 
 public class AlertReceiver extends BroadcastReceiver {
