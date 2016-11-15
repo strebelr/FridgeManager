6ad850903bb74feebc0d5560293abf8136ffaae6
Jeffrey Chen <jeffrey0.0chen@gmail.com>
2016-11-15 15:21:15 -0800

    Minor refractoring

--- app/src/main/java/com/cpen321/fridgemanager/Activity/Instruction.java
+++ app/src/main/java/com/cpen321/fridgemanager/Activity/Instruction.java
@@ -4,12 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import com.cpen321.fridgemanager.R;
 
 import static android.R.id.message;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
