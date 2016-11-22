b60a4c52a07a75d9f3956c8a81646495fae56e65
Xue Wei Wang <vivy.xw.wang@gmail.com>
2016-11-22 12:04:06 -0800

    update timelog

---
+++ app/src/test/java/com/cpen321/fridgemanager/AlertTest.java
@@ -0,0 +1,16 @@
+package com.cpen321.fridgemanager;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by apple on 2016-11-22.
+ */
+public class AlertTest {
+    @Test
+    public void setAlarm() throws Exception {
+
+    }
+
+}
