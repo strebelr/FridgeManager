8397c9f900d2df33a8149dac4404e65bc20c1b93
sang19 <sangeetha_kamath@yahoo.ca>
2016-11-25 20:14:23 -0800

    Changes for add food

--- app/src/main/java/com/cpen321/fridgemanager/Fragment/AddFoodToFoodStockAbbr.java
+++ app/src/main/java/com/cpen321/fridgemanager/Fragment/AddFoodToFoodStockAbbr.java
@@ -1,8 +1,33 @@
 package com.cpen321.fridgemanager.Fragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cpen321.fridgemanager.R;
 
 public class AddFoodToFoodStockAbbr extends Fragment {
+
+    public AddFoodToFoodStockAbbr() {
+
+    }
+
+    private View view;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        this.view = inflater.inflate(R.layout.activity_add_food_enter_abbrv, container, false);
+
+        return view;
+    }
 
 
 
